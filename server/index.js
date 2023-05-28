4c6eda4a7bf40063c2e0366f736b7fbffc05ce58
Abhi Verma <abhiver@Abhis-MacBook-Pro.local>
2023-05-27 22:22:32 -0700

    intake slack events

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ import { Server } from 'socket.io'
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, { cors: { origin: "https://slack-bot-wheat.vercel.app" } });
+const clientUrl = "https://slack-bot-wheat.vercel.app"
+const io = new Server(server, { cors: { origin: clientUrl} });
 const PORT = process.env.PORT || 3000;
 
 const slack_token = process.env.slack_token
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: [clientUrl]}))
 
 
 app.get('/', (req, res) => {
@@ -61,6 +62,11 @@ io.on('connection', (socket) => {
 
   socket.emit('serverEvent', 'Hello from the server!');
 });
+
+app.post('/slackEvent', async (req, res) => {
+  console.log("slackEvent", req.body)
+  res.status(200).send({challenge: req.body.challenge})
+})
 
 
 server.listen(PORT, () => {
